903b9c89325206ba795292110a1fd3aa0c4685cb
Elves Sousa <elves.sousa@gmail.com>
2021-06-14 18:58:48 -0300

    Including styles from outside

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/prismjs@0.0.1/themes/prism-tomorrow.css"
+          />
+          <title>Documentation Starter - Next.js</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
