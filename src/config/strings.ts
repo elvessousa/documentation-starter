87d7d445a8ec4ccb4e3aa1f472d03552a6fa5ae9
Elves Sousa <elves.sousa@gmail.com>
2021-06-14 18:57:02 -0300

    Strings for translation

---
+++ src/config/strings.ts
@@ -0,0 +1,20 @@
+export const LangStrings = {
+  en: {
+    about: 'About',
+    articles: 'Articles',
+    docs: 'Documentation',
+    hello: 'Hello, world!',
+    home: 'Home',
+    inthispage: 'In this page',
+    slogan: 'This is a documentation start for Next.js',
+  },
+  pt: {
+    about: 'Sobre',
+    articles: 'Artigos',
+    docs: 'Documentação',
+    hello: 'Olá, mundo!',
+    home: 'Início',
+    inthispage: 'Nesta página',
+    slogan: 'Este é um modelo de documentação para Next.js.',
+  },
+};
